2ded9e99aaf89b069ed996efb342b5079bd52c8f
Antonio Carlos <antoniocportellavancin1@gmail.com>
2026-06-08 21:12:00 -0300

    Troquei o nome do base atitus, para log.dat

--- recursos/funcoes.py
+++ recursos/funcoes.py
@@ -11,13 +11,13 @@ def aguarde(segundos):
 def inicializarBancoDeDados():
     # r - read, w - write, a - append
     try:
-        banco = open("base.atitus","r")
+        banco = open("log.dat","r")
     except:
         print("Banco de Dados Inexistente. Criando...")
-        banco = open("base.atitus","w")
+        banco = open("log.dat","w")
     
 def escreverDados(nome, pontos):
-    banco = open("base.atitus", "r")
+    banco = open("log.dat", "r")
     dados = banco.read()
     banco.close()
     
@@ -32,14 +32,14 @@ def escreverDados(nome, pontos):
     
     dadosDict[nome] = [pontos, data_br, hora_br]
     
-    banco = open("base.atitus", "w")
+    banco = open("log.dat", "w")
     banco.write(json.dumps(dadosDict))
     banco.close()
     
     # END - inserindo no arquivo
     
 def maior_pontuador():
-    banco = open("base.atitus","r")
+    banco = open("log.dat","r")
     dados = banco.read()
     banco.close()
     if dados != "":
